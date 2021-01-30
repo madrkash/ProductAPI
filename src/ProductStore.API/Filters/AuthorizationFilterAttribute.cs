5b9e44bc5335513680bcc476a40a5b8f71ca9184
Madhu Kashyap <madrkash@outlook.com>
2021-01-30 19:47:25 +1100

    chore: Clean up and renaming

--- src/ProductStore.API/Filters/AuthorizationFilterAttribute.cs
+++ src/ProductStore.API/Filters/AuthorizationFilterAttribute.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-using ProductStore.API.Middleware;
+using ProductStore.API.Configs;
+using System;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace ProductStore.API.Filters
 {
